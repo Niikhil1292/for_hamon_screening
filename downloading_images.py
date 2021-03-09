6425fcfd44398c9c1baefc14358e01f3835bed20
NIKHIL NANDAGOPAN <mails4nikhil123@gmail.com>
2021-03-09 21:15:44 +0530

    screening_problems

--- downloading_images.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-def dl_jpg(url,file_path,file_name):
-    full_path=file_path+file_name+'.jpg'
-    urllib.request.urlretrieve(url,full_path)
-
-url = input("enter the img URL to download:")
-file_name = input("enter the file name:")
-
-dl_jpg(url,'images/',file_name)
